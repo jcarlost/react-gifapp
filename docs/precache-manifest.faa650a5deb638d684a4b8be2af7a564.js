ccf9006adb4598cf77c40a563a8821aa67ab5c1f
jcarlost <jeancarlostoa@gmail.com>
2020-08-22 21:49:56 -0500

    Add docs

---
+++ docs/precache-manifest.faa650a5deb638d684a4b8be2af7a564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78471709251bd4860b330c8b0d63322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3481ff1737dec68343d6",
+    "url": "/static/css/main.d18ac51e.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3481ff1737dec68343d6",
+    "url": "/static/js/main.0b7958e7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
